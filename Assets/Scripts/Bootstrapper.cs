009f325215e27082a65b03a53c2fcec5d1848a39
RShields <playnow254@gmail.com>
2021-08-07 18:32:18 -0700

    Add TargetManager and AIManager ro Bootstrap

--- Assets/Scripts/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper.cs
@@ -5,5 +5,7 @@ public class Bootstrapper : MonoBehaviour
     private void Start()
     {
         gameObject.AddComponent<GridManager>();
+        gameObject.AddComponent<TargetManager>();
+        gameObject.AddComponent<AIManager>();
     }
 }
