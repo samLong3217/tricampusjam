87e10b0539b00528b19b9a8ca8f8da98a3b45057
RShields <playnow254@gmail.com>
2021-08-08 18:08:18 -0700

    Add OnRegister to GridObject

--- Assets/Scripts/Grid/GridObject.cs
+++ Assets/Scripts/Grid/GridObject.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public abstract class GridObject : MonoBehaviour
 {
-    protected void Start()
+    protected virtual void Start()
     {
         Vector3 position = transform.position;
         Location = new Vector2Int(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y));
 
-        if (!GridManager.Register(this)) Destroy(gameObject);
+        if (!GridManager.Register(this))
+        {
+            OnRegister(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            OnRegister(true);
+        }
     }
     
     public Vector2Int Location { get; protected set; }
 
     public abstract IPathfindingNode GetPathfindingNode();
+
+    protected virtual void OnRegister(bool success)
+    {
+        
+    }
 }
