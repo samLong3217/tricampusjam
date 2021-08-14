3671f208c4d9871e57cb4fb6ac4e62cd2eec1c27
Sam Long <sam3217@uw.edu>
2021-08-13 20:33:41 -0700

    Add tower visual deteorating and new walls O_O

--- Assets/Scripts/towers/Tower.cs
+++ Assets/Scripts/towers/Tower.cs
@@ -5,9 +5,18 @@ public class Tower : Wall
     public float Lifespan = 10.0f; // how long it takes for the tower to decay
 
     private float _decayPerSecond;
+    private SpriteRenderer _spriteRenderer;
+    private float _InitLife;
 
     public virtual void Update() {
         TakeDamage(null, _decayPerSecond);
+
+        if (_spriteRenderer != null) {
+            float lifePercent = Health / _InitLife;
+            float addedColor = lifePercent;
+            Color goalColor = new Color(addedColor, addedColor, addedColor, 255);
+            _spriteRenderer.color = goalColor;
+        }
     }
 
     public virtual int GetCost()
@@ -22,8 +31,9 @@ public class Tower : Wall
                 Destroy(gameObject);
                 return;
             }
-            
+            _spriteRenderer = GetComponent<SpriteRenderer>();
             _decayPerSecond = Health / Lifespan * Time.deltaTime;
+            _InitLife = Health;
         } 
     }
 }
